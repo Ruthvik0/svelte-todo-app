6eb202bbac04b892ace4ae9e34e586f5d625c80b
Ruthvik <ruthvikalladi136@gmail.com>
2025-04-05 11:32:15 +0530

    handle empty task

--- src/lib/task-form.svelte
+++ src/lib/task-form.svelte
@@ -1,27 +1,35 @@
 <script lang="ts">
   import { addTask } from "../db/db";
 
-  let newTask : string = $state("");
+  let newTask: string = $state("");
+  let invalidTask: "false" | "true" = $state("false");
 
-  let { loadAllTasks } : {loadAllTasks: () => Promise<void>} = $props()
+  let { loadAllTasks }: { loadAllTasks: () => Promise<void> } = $props();
 
   async function formSubmitted(e: Event) {
     e.preventDefault();
+
     await addTask(newTask);
-    newTask = "";
-    await loadAllTasks()
+    newTask = ""; // Reset the input field after adding
+    await loadAllTasks(); // Load all tasks after adding
   }
+
+  $effect(() => {
+    invalidTask = newTask.trim() === "" ? "true" : "false";
+  });
 </script>
 
 <form onsubmit={formSubmitted}>
   <fieldset role="group">
     <input
-      name="email"
+      id="newTask"
+      name="task"
       type="text"
       bind:value={newTask}
       placeholder="Go get milk"
       autocomplete="off"
+      aria-invalid={invalidTask}
     />
-    <input type="submit" value="Add" />
+    <input disabled={invalidTask === "true"} type="submit" value="Add" />
   </fieldset>
 </form>
